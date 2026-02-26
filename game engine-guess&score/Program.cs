58954b0a5bbe45eec93f6145b102a7554c3ec56e
Eslam-Azooz <eslamazooz23@gmail.com>
2026-02-26 09:58:39 +0200

    Add project files.

---
+++ game engine-guess&score/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace game_engine_guess_score
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
