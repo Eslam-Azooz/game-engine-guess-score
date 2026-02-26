c77216977bc24662ab1ec68e9c168f88f9f9c287
Eslam-Azooz <eslamazooz23@gmail.com>
2026-02-26 10:54:03 +0200

    class round to number of try

--- game engine-guess&score/round.cs
+++ game engine-guess&score/round.cs
@@ -22,55 +22,55 @@ namespace game_engine_guess_score
             Console.WriteLine($"enter number of try  ");
             trynumber= int .Parse(Console.ReadLine());
 
-            whileloop();
+           // whileloop();
         }
 
-        public void whileloop()
-        {
-            Random rnd = new Random();
-            int num=rnd.Next(1, 6);
+        //public void whileloop()
+        //{
+        //    Random rnd = new Random();
+        //    int num=rnd.Next(1, 6);
           
-            while (trynumber != 0)
-            {
-                int number;
-                Console.WriteLine($"enter number between 1 and 5  ");    
+        //    while (trynumber != 0)
+        //    {
+        //        int number;
+        //        Console.WriteLine($"enter number between 1 and 5  ");    
 
-                number = int.Parse(Console.ReadLine());
+        //        number = int.Parse(Console.ReadLine());
 
-                if (number != num)
-                {
-                    score -= 30;
-                }
-                trynumber--;
-            }
-            if (score != 0)
-            {
-                Console.WriteLine($"you win with score {score}");
-            }
-            else
-            {
-                Console.WriteLine($"you lose with score {score}");
-            }
-        }
+        //        if (number != num)
+        //        {
+        //            score -= 30;
+        //        }
+        //        trynumber--;
+        //    }
+        //    if (score != 0)
+        //    {
+        //        Console.WriteLine($"you win with score {score}");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine($"you lose with score {score}");
+        //    }
+        //}
 
-        public void replay()
-        {
-            Console.WriteLine($"do you want to play again? (y/n) ");
-            char ch= char.Parse(Console.ReadLine());    
-            if (ch == 'y')
-            {
-                roundnumber++;
-                roundstart();
-            }
-            else
-            {
-                Console.WriteLine($"thank you for playing!");
-                return;
-            }
+        //public void replay()
+        //{
+        //    Console.WriteLine($"do you want to play again? (y/n) ");
+        //    char ch= char.Parse(Console.ReadLine());    
+        //    if (ch == 'y')
+        //    {
+        //        roundnumber++;
+        //        roundstart();
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine($"thank you for playing!");
+        //        return;
+        //    }
 
 
 
-        } 
+        //} 
             
 
             
