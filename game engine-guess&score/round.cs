4b94103649006f78f0d5b53e3efd507bdf64dfbd
Eslam-Azooz <eslamazooz23@gmail.com>
2026-02-27 18:05:57 +0200

    comment on function while loop in calss round

--- game engine-guess&score/round.cs
+++ game engine-guess&score/round.cs
@@ -27,33 +27,33 @@ namespace game_engine_guess_score
             replay();
         }
 
-        public void whileloop()
-        {
-            Random rnd = new Random();
-            int num = rnd.Next(1, 6);
+        //public void whileloop()
+        //{
+        //    Random rnd = new Random();
+        //    int num = rnd.Next(1, 6);
 
-            while (trynumber != 0)
-            {
-                int number;
-                Console.WriteLine($"enter number between 1 and 5  ");
+        //    while (trynumber != 0)
+        //    {
+        //        int number;
+        //        Console.WriteLine($"enter number between 1 and 5  ");
 
-                number = int.Parse(Console.ReadLine());
+        //        number = int.Parse(Console.ReadLine());
 
-                if (number != num)
-                {
-                    score -= 30;
-                }
-                trynumber--;
-            }
-            if (score >= 0)
-            {
-                Console.WriteLine($"you win with score {score}");
-            }
-            else 
-            {
-                Console.WriteLine($"you lose with score {0}");
-            }
-        }
+        //        if (number != num)
+        //        {
+        //            score -= 30;
+        //        }
+        //        trynumber--;
+        //    }
+        //    if (score >= 0)
+        //    {
+        //        Console.WriteLine($"you win with score {score}");
+        //    }
+        //    else 
+        //    {
+        //        Console.WriteLine($"you lose with score {0}");
+        //    }
+        //}
 
         public void replay()
         {
